12c8475f06eb8b3d7a0bdca91acd7eb7b4b4f969
Craig Peterson <craig.peterson0701@gmail.com>
2023-12-24 12:38:37 -0600

    Update plugins_init.lua

--- plugins_init.lua
+++ plugins_init.lua
@@ -286,4 +286,17 @@ if #config.plugins > 0 then
   table.insert(default_plugins, { import = config.plugins })
 end
 
-require("lazy").setup(default_plugins, config.lazy_nvim)
+require("lazy").setup({
+  { default_plugins, config.lazy_nvim },
+  { 'codota/tabnine-nvim', build = "./dl_binaries.sh" }
+})
+
+require('tabnine').setup({
+  disable_auto_comment=true,
+  accept_keymap="<Tab>",
+  dismiss_keymap = "<C-]>",
+  debounce_ms = 800,
+  suggestion_color = {gui = "#808080", cterm = 244},
+  exclude_filetypes = {"TelescopePrompt", "NvimTree"},
+  log_file_path = nil, -- absolute path to Tabnine log file
+})
