8bbe3325f66df3051c6afab655e5246b66b7f361
Craig Peterson <craig.peterson0701@gmail.com>
2024-01-23 20:54:04 -0600

    Add files via upload

---
+++ Lua/custom/configs/null-ls.lua
@@ -0,0 +1,38 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_ls = require("null-ls")
+
+local opts = {
+  sources = {
+    -- webdev stuff
+    null_ls.builtins.formatting.deno_fmt,
+    null_ls.builtins.formatting.prettier.with { filetypes = { "html", "markdown", "css" } },
+    -- Lua
+    null_ls.builtins.formatting.stylua,
+    -- cpp
+    null_ls.builtins.formatting.clang_format,
+    -- python
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.ruff,
+    -- go
+    null_ls.builtins.formatting.gofumpt,
+    null_ls.builtins.formatting.goimports_reviser,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr})
+        end,
+      })
+    end
+  end
+}
+
+return opts
---
+++ Lua/custom/mappings.lua
@@ -0,0 +1,32 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    ['dl'] = {"dd", "delete current line" },
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+  },
+  v = {
+    [">"] = { ">gv", "indent" },
+  },
+}
+
+-- more keybinds!
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start of continue the debugger",
+    }
+  }
+}
+
+return M
